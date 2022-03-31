334dc13701bff4dcbad42f058be33eb8ad5153de
liuhuan <liuhuan@gigacloudtech.com>
2022-03-31 13:13:00 +0800

    init-验证码生成和校验

---
+++ permission-server/permission-server-system/src/main/java/com/frog/PermissionServerSystemApplication.java
@@ -0,0 +1,13 @@
+package com.frog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PermissionServerSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PermissionServerSystemApplication.class, args);
+    }
+
+}
